6c9eee17d5cf8869222a9ec23c87f0d32ee24641
Eric Sorum <ejsorum@gmail.com>
2022-08-10 10:27:55 -0500

    Trying HashRouter.

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Work from './pages/Work';
 import Studio from './pages/Studio';
@@ -6,10 +6,12 @@ import Team from './pages/Team';
 import Bulletin from './pages/Bulletin';
 import Contact from './pages/Contact';
 
+//NEXT try using HashRouter... may not work locally but might work on GH Pages
+// Or try finding another hosting site lol
 
 const RouteSwitch = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/route-practice" element={<Home />} />
         <Route path="/route-practice/work" element={<Work />} />
@@ -18,7 +20,7 @@ const RouteSwitch = () => {
         <Route path="/route-practice/bulletin" element={<Bulletin />} />
         <Route path="/route-practice/contact" element={<Contact />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
