a97da8a8fbe13f84edfe08a5341ee84364e445b7
Eric Sorum <ejsorum@gmail.com>
2022-08-10 11:34:48 -0500

    Going back to BrowserRouter.

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Work from './pages/Work';
 import Studio from './pages/Studio';
@@ -11,16 +11,16 @@ import Contact from './pages/Contact';
 
 const RouteSwitch = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
-        <Route path="/route-practice" element={<Home />} />
-        <Route path="/route-practice/work" element={<Work />} />
-        <Route path="/route-practice/studio" element={<Studio />} />
-        <Route path="/route-practice/team" element={<Team />} />
-        <Route path="/route-practice/bulletin" element={<Bulletin />} />
-        <Route path="/route-practice/contact" element={<Contact />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/work" element={<Work />} />
+        <Route path="/studio" element={<Studio />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/bulletin" element={<Bulletin />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
